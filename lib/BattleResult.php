f71e448e857eeb76e8c9955d66c877f2bc54a630
Brad <bgreen3@uoregon.edu>
2017-04-16 20:06:03 -0600

    Add function to check if there was a winner

--- lib/BattleResult.php
+++ lib/BattleResult.php
@@ -8,26 +8,40 @@ class BattleResult
 
   private $losingShip;
 
-  public function __construct($usedJediPowers, Ship $winningShip, Ship $losingShip)
+  public function __construct($usedJediPowers, Ship $winningShip = null, Ship $losingShip = null)
   {
     $this->usedJediPowers = $usedJediPowers;
     $this->winningShip = $winningShip;
     $this->losingShip = $losingShip;
   }
 
+  /**
+  * @return boolean
+  */
   public function wereJediPowersUsed()
   {
     return $this->usedJediPowers;
   }
 
+  /**
+  * @return Ship|null
+  */
   public function getWinningShip()
   {
     return $this->winningShip;
   }
 
+  /**
+  * @return Ship|null
+  */
   public function getLosingShip()
   {
     return $this->losingShip;
+  }
+
+  public function isThereAWinner()
+  {
+    return $this->getWinningShip() !== null;
   }
 
 }
